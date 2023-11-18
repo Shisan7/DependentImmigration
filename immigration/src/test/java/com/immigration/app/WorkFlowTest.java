05ae4ea9aa750c23f940d5f453f5db3297f80976
Seasun <67022429+Nano8755@users.noreply.github.com>
2023-11-17 20:15:45 -0500

    Integrated the reviewer and UI class
    Integrated the reviewer and UI class and also fixed the errors with the workflowtest class

--- immigration/src/test/java/com/immigration/app/WorkFlowTest.java
+++ immigration/src/test/java/com/immigration/app/WorkFlowTest.java
@@ -14,21 +14,21 @@ public class WorkFlowTest {
     @Test
     public void testAddReadyApp() {
         DependentAdd da = new DependentAdd(null, 0); // Create a sample DependentAdd object
-        assertTrue(workFlow.addReadyApp(da)); // Test if adding to readyQueue returns true
+        assertTrue(workFlow.saveReadyQueue(da)); // Test if adding to readyQueue returns true
         assertEquals(1, workFlow.readyQueue.size()); // Test if the queue size increased
     }
 
     @Test
     public void testAddTerminatedApp() {
         DependentAdd da = new DependentAdd(null, 0); // Create a sample DependentAdd object
-        assertTrue(workFlow.addTerminatedApp(da)); // Test if adding to terminatedList returns true
+        assertTrue(workFlow.saveTerminatedApp(da)); // Test if adding to terminatedList returns true
         assertEquals(1, workFlow.terminatedList.size()); // Test if the list size increased
     }
 
     @Test
     public void testAddCompletedApp() {
         DependentAdd da = new DependentAdd(null, 0); // Create a sample DependentAdd object
-        assertTrue(workFlow.addCompletedApp(da)); // Test if adding to completedList returns true
+        assertTrue(workFlow.saveCompletedApp(da)); // Test if adding to completedList returns true
         assertEquals(1, workFlow.completedList.size()); // Test if the list size increased
     }
 
@@ -38,14 +38,8 @@ public class WorkFlowTest {
     }
 
     @Test
-    public void testSaveReviewer() {
-        Reviewer reviewer = new Reviewer(workFlow); // Create a sample Reviewer object
-        assertNotNull(workFlow.saveReviewer(reviewer)); // Assuming saveReviewer returns null
-    }
-
-    @Test
     public void testSaveApplication() {
         DataEntry de = new DataEntry(); // Create a sample DependentAdd object
-        assertNotNull(workFlow.saveApplication(de)); // Assuming saveApplication returns null
+        assertNotNull(workFlow.saveApplication(workFlow.inProgressUser)); // Assuming saveApplication returns null
     }
 }
